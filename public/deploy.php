ff4ae96940a56b5b6734910ce3a48e0d012947af
Robert Mihai Colca <robery_office@yahoo.ro>
2018-04-07 17:21:20 +0300

    Added deploy script

---
+++ public/deploy.php
@@ -0,0 +1,41 @@
+<?php
+$commands = array(
+    'echo $PWD',
+    'whoami',
+    'git pull',
+    'git status',
+    'git submodule sync',
+    'git submodule update',
+    'git submodule status',
+    'composer update'
+);
+
+// Run the commands for output
+$output = '';
+foreach($commands AS $command){
+    // Run it
+    $tmp = shell_exec($command);
+    // Output
+    $output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+    $output .= htmlentities(trim($tmp)) . "\n";
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <title>GIT DEPLOYMENT SCRIPT</title>
+    </head>
+    <body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+        <pre>
+         .  ____  .    ____________________________
+         |/      \|   |                            |
+        [| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git Deployment Script v0.1 |
+         |___==___|  /                             |
+                      |____________________________|
+
+            <?php echo $output; ?>
+        </pre>
+    </body>
+</html>
