92cd163a6da786d5def3c7dcfd7351bdb1f91001
Robert Mihai Colca <robery_office@yahoo.ro>
2018-05-17 11:39:28 +0300

    Created the home controller (umm... things are getting to work)

---
+++ app/src/Component/Home/Controller.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: robery567
+ * Date: 17/05/2018
+ * Time: 11:36
+ */
+
+class Component_Home_Controller {
+    public function indexAction() {
+        return 'sth';
+    }
+}
