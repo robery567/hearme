b2b34972320eca686432b51f7e58300f9b6b5bf4
Robert Mihai Colca <robery_office@yahoo.ro>
2018-05-21 18:07:56 +0300

    Throwing exception when no file has been while trying to autoload

--- app/autoload.php
+++ app/autoload.php
@@ -20,7 +20,7 @@ function autoload($className) {
     if (strpos(strtolower($fileName), 'symfony') === false) {
         $relativePathToFile = 'src/' . $fileName;
 
-        if (stream_resolve_include_path($relativePathToFile) === true) {
+        if (stream_resolve_include_path($relativePathToFile) === false) {
             throw new Exception("Component not found");
         }
 
