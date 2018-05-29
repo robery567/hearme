012da9f09bb7da4477a9fbdacce3949c47bc9e2c
Robert Colca <rco@newpharma.ro>
2018-05-29 12:24:55 +0300

    Showing friends in tree listing

--- app/src/Module/Tree/Printer/Model.php
+++ app/src/Module/Tree/Printer/Model.php
@@ -149,7 +149,7 @@ class Module_Tree_Printer_Model {
                                     <li><strong>Gender:</strong> {$userData['gender']}</li>
                                     <li><strong>Friends:</strong>";
 
-        var_dump($userData);
+        var_dump(json_decode($userData['friends']));
         $this->dataToRender .= $userData['friends'];
 
         $this->dataToRender .= '</li>
